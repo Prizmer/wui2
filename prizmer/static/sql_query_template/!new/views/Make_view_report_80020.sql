eddc4dab2b8f0fed86173f1522db006b08faf705
Lena <semercidis@gmail.ru>
2018-10-09 12:26:25 +0300

    не зафиксированные view

---
+++ prizmer/static/sql_query_template/!new/views/Make_view_report_80020.sql
@@ -0,0 +1,18 @@
+﻿CREATE OR REPLACE VIEW report_80020 AS 
+SELECT 
+                          groups_80020.name_sender, 
+                          groups_80020.inn_sender, 
+                          groups_80020.dogovor_number, 
+                          meters.factory_number_manual, 
+                          link_groups_80020_meters.measuringpoint_name, 
+                          link_groups_80020_meters.measuringpoint_code, 
+                          meters.dt_last_read,
+                          groups_80020.name as group_name
+                        FROM 
+                          public.meters, 
+                          public.groups_80020, 
+                          public.link_groups_80020_meters
+                        WHERE 
+                          link_groups_80020_meters.guid_meters = meters.guid AND
+                          link_groups_80020_meters.guid_groups_80020 = groups_80020.guid AND
+                          groups_80020.name = '80020'
